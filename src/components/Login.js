70ba302bf08144328a7e2d132b0d72510770be8e
github username <github email address>
2018-06-29 14:32:09 -0400

    NEW PROJECT--something went really wrong with last file

---
+++ src/components/Login.js
@@ -0,0 +1,37 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+const Login = props => {
+
+}
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Login"
+           />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/containers/Mainpage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Mainpage extends Component {
+  render() {
+    return (
+      <div>What Up</div>
+    )
+  }
+}
+
+export default Mainpage;
