c4e925382fbefe8428221a2252350bac890bc196
github username <github email address>
2018-07-03 10:38:20 -0400

    fixed conditional rendering of navBar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -3,29 +3,17 @@ import Register from '../Components/Register';
 import Login from '../Components/Login';
 import { Input, Menu } from 'semantic-ui-react'
 import logo from '../images/logo.svg';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 class NavBar extends Component {
   state = {
     activeItem: '',
   }
 
-  switchRenderFunction = () => {
-    switch(this.state.activeItem) {
-      case 'signup':
-      return (
-        <Register />
-      )
-      case 'login':
-      return (
-        <Login />
-      )
-    }
-  }
-
   handleItemClick = (e, { name }) => {
     this.setState({
       activeItem: name
-    },()=>{this.switchRenderFunction()})
+    })
   }
 
   redirectToSignup = () => {
@@ -41,30 +29,30 @@ class NavBar extends Component {
         <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}>
         <img src={logo} alt="home button"/>
         </Menu.Item>
-        <Menu.Item
-          name='matches'
-          active={activeItem === 'matches'}
-          onClick={this.handleItemClick}
-        />
+        { localStorage.getItem('token') ?
+          <Menu.Item
+            name='matches'
+            active={activeItem === 'matches'}
+            onClick={this.handleItemClick}
+          />
+        :
+        null }
         <Menu.Menu position='right'>
           <Menu.Item>
             <Input icon='search' placeholder='Search...' />
           </Menu.Item>
+         { localStorage.getItem('token') ?
+           <Menu.Item
+            name='logout'
+            active={activeItem === 'logout'}
+            onClick={this.handleItemClick}
+            />
+          :
           <Menu.Item
             name='signup'
             active={activeItem === 'signup'}
             onClick={this.redirectToSignup}
-          />
-          <Menu.Item
-            name='login'
-            active={activeItem === 'login'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='logout'
-            active={activeItem === 'logout'}
-            onClick={this.handleItemClick}
-          />
+          />}
         </Menu.Menu>
       </Menu>
     )
